e1f2e73fe01ac3396be83345e78f8f3e3ee4a14d
RafaViBi <rafavb12@gmail.com>
2018-09-24 16:22:39 +0200

    Ejercicio 3 Terminado

--- DWEC/1Eval/Tema1/JS/Ejercicio3.js
+++ DWEC/1Eval/Tema1/JS/Ejercicio3.js
@@ -1,15 +1,54 @@
 var DNI = prompt('Dime tu DNI');
-var correo
-var telefono
-var dirIP
-var URL
+var correo = prompt('Dime tu correo');
+var telefono = prompt('Dime tu telefono');
+var dirIP = prompt('Dime tu direccion IP');
+var URL = prompt('Dime tu URL'); 
+
+
+if (DNICorrecto(DNI)) {
+    document.write("El DNI es correcto.");
+} else {
+    document.write("El DNI no es correcto.");
+} 
+
+
+
+if (correoCorrecto(correo)) {
+    document.write(" <br> " + "El correo electronico es correcto.");
+} else {
+    document.write(" <br> " + "El correo electronico no es correcto.");
+} 
+
+
+
+if (telCorrecto(telefono)) {
+    document.write(" <br> " + "El telefono es correcto.");
+} else {
+    document.write(" <br> " + "El telefono no es correcto.");
+} 
+
+
+
+if (dirIPCorrecta(dirIP)) {
+    document.write(" <br> " + "La dirección IP es correcta.");
+} else {
+    document.write(" <br> " + "La dirección IP no es correcta.");
+}
+
+
+
+if (urlCorrecta(URL)) {
+    document.write(" <br> " + "La URL es correcta.");
+} else {
+    document.write(" <br> " + "La URL no es correcta.");
+}
 
 function DNICorrecto(DNI) {
     
     var numero
     var letra
     var ExpDNI =/^[0-9]{8}[a-zA-Z]{1}/;
-    DNI = DNI.toUpperCase;
+    dni = DNI.toUpperCase;
 
     if(ExpDNI.test(dni) == true){
         
@@ -21,18 +60,34 @@ function DNICorrecto(DNI) {
 
         if (letra!=letr) {
 
-            document.write('Tu DNI no es correcto ');
+            return false;
 
         }else{
 
-            document.write('Dni correcto');
+            return true;
 
         }
 
     }else{
 
-        document.write('Dni erroneo, formato no válido');
+        return false;
 
     }
 
+}
+
+function correoCorrecto(correo) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(correo);
+}
+
+function telCorrecto(telefono) {
+    return /^(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}$/.test(telefono);
+}
+
+function dirIPCorrecta(dirIP) {
+    return /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/.test(dirIP);
+}
+
+function urlCorrecta(URL) {
+    return /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})$/.test(URL);
 }

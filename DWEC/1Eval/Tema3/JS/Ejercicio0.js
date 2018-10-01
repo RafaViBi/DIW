9f4f07c0579c60315082a699fa71d1910a5c8fed
RafaViBi <rafavb12@gmail.com>
2018-10-01 19:45:36 +0200

    Ejercicio 0 inicio

---
+++ DWEC/1Eval/Tema3/JS/Ejercicio0.js
@@ -0,0 +1,14 @@
+//nombre, dirección, dni, teléfono,libro, título, autor, país, isbn;
+
+
+class MiClase {};
+let miClase = new MiClase;
+
+class MiClase {
+    constructor ( x = 0, y = 0 ) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+let p1 = new MiClase( 10,20 );

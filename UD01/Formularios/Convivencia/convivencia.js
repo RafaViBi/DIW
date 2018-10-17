b997ee01184761baa5f7e785fab30ffa1f9be547
RafaViBi <rafavb12@gmail.com>
2018-10-17 19:03:16 +0200

    retocando convivencia

--- UD01/Formularios/Convivencia/convivencia.js
+++ UD01/Formularios/Convivencia/convivencia.js
@@ -1,6 +1,16 @@
-function onSubmit( form ){
-    
-    var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
-    console.log( data );
-    return false; //don't submit
-  }
+function onSubmit(form) {
+
+  var data = JSON.stringify($(form).serializeArray()); //  <-----------
+  console.log(data);
+  return false; //don't submit
+}
+
+function visibilidadLeve() {
+  document.getElementById("leve").style.visibility="visible";
+  document.getElementById("grave").style.visibility="hidden";
+}
+
+function visibilidadGrave() {
+  document.getElementById("grave").style.visibility="visible";
+  document.getElementById("leve").style.visibility="hidden";
+}

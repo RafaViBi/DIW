cc191c85e04b1b2267565df122231f8622b4bbc0
RafaViBi <rafavb12@gmail.com>
2018-11-13 19:01:28 +0100

    Arreglar el bucle al jugar CPU

---
+++ DWEC/1Eval/BOM/Ejercicio03/Ejercicio03.js
@@ -0,0 +1,135 @@
+window.addEventListener("load", cargar, false);
+
+var jugador = 1;
+var celdas = ["1","2","3","4","5","6","7","8","9"];
+
+function cargar() {
+    document.getElementById("celda1").addEventListener("click", jugar);
+    document.getElementById("celda2").addEventListener("click", jugar);
+    document.getElementById("celda3").addEventListener("click", jugar);
+    document.getElementById("celda4").addEventListener("click", jugar);
+    document.getElementById("celda5").addEventListener("click", jugar);
+    document.getElementById("celda6").addEventListener("click", jugar);
+    document.getElementById("celda7").addEventListener("click", jugar);
+    document.getElementById("celda8").addEventListener("click", jugar);
+    document.getElementById("celda9").addEventListener("click", jugar);
+}
+
+function jugar() {
+    if (jugador == 1) {
+        if (this.innerHTML == "") {
+            this.innerHTML = "X";
+            jugador++;
+            comprobarVictoriaJugador(this);
+            jugarCPU();
+        }
+    }
+}
+
+function comprobarVictoriaCPU() {
+
+    //HORIZONTAL
+    if (document.getElementById("celda1").innerHTML == "O" && document.getElementById("celda2").innerHTML == "O" && document.getElementById("celda3").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+
+    if (document.getElementById("celda4").innerHTML == "O" && document.getElementById("celda5").innerHTML == "O" && document.getElementById("celda6").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+
+    if (document.getElementById("celda7").innerHTML == "O" && document.getElementById("celda8").innerHTML == "O" && document.getElementById("celda9").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+
+    //DIAGONAL
+    if (document.getElementById("celda1").innerHTML == "O" && document.getElementById("celda5").innerHTML == "O" && document.getElementById("celda9").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+
+    if (document.getElementById("celda3").innerHTML == "O" && document.getElementById("celda5").innerHTML == "O" && document.getElementById("celda7").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+
+    //VERTICAL
+    if (document.getElementById("celda2").innerHTML == "O" && document.getElementById("celda4").innerHTML == "O" && document.getElementById("celda1").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+
+    if (document.getElementById("celda2").innerHTML == "O" && document.getElementById("celda5").innerHTML == "O" && document.getElementById("celda8").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+
+    if (document.getElementById("celda3").innerHTML == "O" && document.getElementById("celda6").innerHTML == "O" && document.getElementById("celda9").innerHTML == "O") {
+        alert("Victoria CPU");
+        location.reload();
+    }
+}
+
+function comprobarVictoriaJugador() {
+
+    //HORIZONTAL
+    if (document.getElementById("celda1").innerHTML == "X" && document.getElementById("celda2").innerHTML == "X" && document.getElementById("celda3").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+
+    if (document.getElementById("celda4").innerHTML == "X" && document.getElementById("celda5").innerHTML == "X" && document.getElementById("celda6").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+
+    if (document.getElementById("celda7").innerHTML == "X" && document.getElementById("celda8").innerHTML == "X" && document.getElementById("celda9").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+
+    //DIAGONAL
+    if (document.getElementById("celda1").innerHTML == "X" && document.getElementById("celda5").innerHTML == "X" && document.getElementById("celda9").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+
+    if (document.getElementById("celda3").innerHTML == "X" && document.getElementById("celda5").innerHTML == "X" && document.getElementById("celda7").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+
+    //VERTICAL
+    if (document.getElementById("celda2").innerHTML == "X" && document.getElementById("celda4").innerHTML == "X" && document.getElementById("celda1").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+
+    if (document.getElementById("celda2").innerHTML == "X" && document.getElementById("celda5").innerHTML == "X" && document.getElementById("celda8").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+
+    if (document.getElementById("celda3").innerHTML == "X" && document.getElementById("celda6").innerHTML == "X" && document.getElementById("celda9").innerHTML == "X") {
+        alert("Victoria Jugador");
+        location.reload();
+    }
+}
+
+function jugarCPU() {
+    
+    var eleccionCPU = Math.floor(Math.random() * (celdas.length - 1) + 1);
+    
+    console.log(eleccionCPU);
+
+    if (document.getElementById("celda" + eleccionCPU).innerHTML == "") {
+        document.getElementById("celda" + eleccionCPU).innerHTML = "O";
+        jugador--;
+        celdas.splice(1,eleccionCPU);
+        comprobarVictoriaCPU(this);
+    } else {
+        jugarCPU();
+    }
+}

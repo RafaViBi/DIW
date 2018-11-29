4e00ed8a892d73113c64c1a27fed15cee6f14daa
RafaViBi <rafavb12@gmail.com>
2018-11-29 15:25:29 +0100

    go to a modificar incidencia

--- UD01/Formularios/convivenciaServer/public/convivencia.js
+++ UD01/Formularios/convivenciaServer/public/convivencia.js
@@ -1,9 +1,9 @@
 function visibilidadLeve() {
-  document.getElementById("leve").style.visibility="visible";
-  document.getElementById("grave").style.visibility="hidden";
+  document.getElementById("leve").style.visibility = "visible";
+  document.getElementById("grave").style.visibility = "hidden";
 }
 
 function visibilidadGrave() {
-  document.getElementById("grave").style.visibility="visible";
-  document.getElementById("leve").style.visibility="hidden";
+  document.getElementById("grave").style.visibility = "visible";
+  document.getElementById("leve").style.visibility = "hidden";
 }

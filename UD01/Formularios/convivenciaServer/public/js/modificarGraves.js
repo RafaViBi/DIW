12151f40f5a30b2f8717a4a3e735d9eebda02e34
RafaViBi <rafavb12@gmail.com>
2018-12-03 16:23:39 +0100

    por fin cojo la id bn VAMOOOOOS

--- UD01/Formularios/convivenciaServer/public/js/modificarGraves.js
+++ UD01/Formularios/convivenciaServer/public/js/modificarGraves.js
@@ -1,11 +1,23 @@
-$.getJSON("http://localhost:3000/convivenciaGrave", function (data) {
+auxId = location.search.substring(1,location.search.length);  
+
+$.getJSON("http://localhost:3000/convivencia/"+auxId, function (data) {
   var items = [];
   $.each(data, function (key, val) {
-    items.push("<input name='nombreAlumno' type='text' class='form-control' id='alumno' value=" + val.nombreAlumno + ">");
+    items.push("<div class='form-group row'>");
+      items.push("<label for='alumno' class='col-sm-2 col-form-label'>ALUMNE/A</label>");
+      items.push("<div class='col-sm-7'>");
+        items.push("<input name='nombreAlumno' type='text' class='form-control' id='alumno' value=" + val.nombreAlumno + ">");
+      items.push("</div>");
+      items.push("<label for='grupo' class='col-sm-1 col-form-label'>GRUP</label>");
+      items.push("<div class='col-sm-2'>");
+        items.push("<input name='grupoAlumno' type='text' class='form-control' id='grupo' value=" + val.grupoAlumno + ">");
+      items.push("</div>");
+    items.push("</div>");
   });
 
-  $("<table/>", {
-    "class": "table table-striped table-dark",
+  $("<form/>", {
+    "method": "POST",
+    "action": "http://localhost:3000/convivencia/",
     html: items.join("")
-  }).appendTo("#divListaGraves");
+  }).appendTo("#modificarFalta");
 });

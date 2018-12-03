2fb46a5433d7b2540060eef6ba010c896bf696b6
RafaViBi <rafavb12@gmail.com>
2018-12-03 16:53:18 +0100

    a puntico de terminar modificar

--- UD01/Formularios/convivenciaServer/public/js/modificarGraves.js
+++ UD01/Formularios/convivenciaServer/public/js/modificarGraves.js
@@ -1,13 +1,28 @@
-auxId = location.search.substring(1,location.search.length);
-ruta = "http://localhost:3000/convivencia/"+auxId;
+auxId = location.search.substring(1, location.search.length);
+ruta = "http://localhost:3000/convivencia/" + auxId;
+var camposMostrar = ["nombreAlumno", "grupoAlumno", "nombreProfesor", "horarioAtencion", "fechaIncidente", "horaIncidente", "lugarIncidente", "telefonoPadres", "emailPadres", "fechaHoy"];
 
 $.getJSON(ruta, function (data) {
   var items = [];
   $.each(data, function (key, val) {
-    items.push("<label for='alumno' class='col-sm-2 col-form-label'>ALUMNE/A</label>");
+    if (camposMostrar.includes(key)) {
+      items.push("<label>" + key + "</label><br>");
+      items.push("<input value='" + val + "'></input> <input type='submit' onclick='renovarCampo()'> <br>");
+    }
   });
 
   $("<div/>", {
     html: items.join("")
   }).appendTo("#modificarFalta");
 });
+
+/* function renovarCampo(auxId) {
+  $.ajax({
+    type: "PUT",
+    url: "http://localhost:3000/convivencia/" + auxId,
+    data: "",
+  });
+  setInterval(function () {
+    window.location.reload();
+  }, 50);
+} */

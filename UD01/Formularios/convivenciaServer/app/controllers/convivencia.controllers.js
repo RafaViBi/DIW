f31ed079d07dca35ede3e841d04e19dd2f2f788d
RafaViBi <rafavb12@gmail.com>
2018-12-13 16:39:06 +0100

    Intentar modificar campos pero no va :'(

--- UD01/Formularios/convivenciaServer/app/controllers/convivencia.controllers.js
+++ UD01/Formularios/convivenciaServer/app/controllers/convivencia.controllers.js
@@ -14,32 +14,32 @@ exports.create = (req, res) => {
 
     if (req.body.tipoConvivencia == "leve") {
         const convivencia = new convivenciaLeve({
-            nombreAlumno: req.body.nombreAlumno || "No nombre",
-            grupoAlumno: req.body.grupoAlumno || "Sin grupo",
-            nombreProfesor: req.body.nombreProfesor || "No nombre Profesor",
-            horarioAtencion: req.body.horarioAtencion || "No hora atencion",
-            fechaIncidente: req.body.fechaIncidente || "0000/00/00",
-            horaIncidente: req.body.horaIncidente || "No nombre",
-            lugarIncidente: req.body.lugarIncidente || "No nombre",
+            nombreAlumno: req.body.nombreAlumno || "Alumno no indicado",
+            grupoAlumno: req.body.grupoAlumno || "Grupo no indicado",
+            nombreProfesor: req.body.nombreProfesor || "Nombre profesor no indicado",
+            horarioAtencion: req.body.horarioAtencion || "Horario atencion no indicado",
+            fechaIncidente: req.body.fechaIncidente || "0000-00-00",
+            horaIncidente: req.body.horaIncidente || "Hora indicente no indicada",
+            lugarIncidente: req.body.lugarIncidente || "Lugar indicente no indicada",
             opcionA: comprobarBoolean(req.body.opcionA) || false,
             opcionB: comprobarBoolean(req.body.opcionB) || false,
             opcionC: comprobarBoolean(req.body.opcionC) || false,
             opcionD: comprobarBoolean(req.body.opcionD) || false,
-            fechaTareaInicio: req.body.fechaTareaInicio || "0000/00/00",
-            fechaTareaFin: req.body.fechaTareaFin || "0000/00/00",
-            horaTareaInicio: req.body.horaTareaInicio || "No nombre",
-            horaTareaFin: req.body.horaTareaFin || "No nombre",
+            fechaTareaInicio: req.body.fechaTareaInicio || "0000-00-00",
+            fechaTareaFin: req.body.fechaTareaFin || "00000-00-00",
+            horaTareaInicio: req.body.horaTareaInicio || "Hora inicio tarea no indicada",
+            horaTareaFin: req.body.horaTareaFin || "Hora fin tarea no indicada",
             opcionE: comprobarBoolean(req.body.opcionE) || false,
             opcionF: comprobarBoolean(req.body.opcionF) || false,
-            fechaSuspActInicio: req.body.fechaSuspActInicio || "0000/00/00",
-            fechaSuspActFin: req.body.fechaSuspActFin || "0000/00/00",
-            fechaSuspAsisInicioF: req.body.fechaSuspAsisInicioF || "0000/00/00",
-            fechaSuspAsisFinF: req.body.fechaSuspAsisFinF || "0000/00/00",
-            descripcionIncidente: req.body.descripcionIncidente || "No nombre",
-            tipificacion: req.body.tipificacion || "No nombre",
-            telefonoPadres: req.body.telefonoPadresL || "No nombre",
-            emailPadres: req.body.emailPadresL || "No nombre",
-            fechaHoy: req.body.fechaHoyL || "0000/00/00"
+            fechaSuspActInicio: req.body.fechaSuspActInicio || "0000-00-00",
+            fechaSuspActFin: req.body.fechaSuspActFin || "0000-00-00",
+            fechaSuspAsisInicioF: req.body.fechaSuspAsisInicioF || "0000-00-00",
+            fechaSuspAsisFinF: req.body.fechaSuspAsisFinF || "0000-00-00",
+            descripcionIncidente: req.body.descripcionIncidente || "Descripcion no indicada",
+            tipificacion: req.body.tipificacion || "Tipificacion no seleccionada",
+            telefonoPadres: req.body.telefonoPadresL || "Telefono padres no indicado",
+            emailPadres: req.body.emailPadresL || "Email no indicado",
+            fechaHoy: req.body.fechaHoyL || "0000-00-00"
         })
 
         convivencia.save().then(data => {
@@ -54,18 +54,17 @@ exports.create = (req, res) => {
 
     if (req.body.tipoConvivencia == "grave") {
         const convivencia = new convivenciaGrave({
-            nombreAlumno: req.body.nombreAlumno || "No nombre",
-            grupoAlumno: req.body.grupoAlumno || "No nombre",
-            nombreProfesor: req.body.nombreProfesor || "No nombre",
-            horarioAtencion: req.body.horarioAtencion || "No nombre",
-            fechaIncidente: req.body.fechaIncidente || "00/00/0000",
-            horaIncidente: req.body.horaIncidente || "No nombre",
-            lugarIncidente: req.body.lugarIncidente || "No nombre",
-            tipificacion: req.body.tipificacion || "No nombre",
-            
-            telefonoPadres: req.body.telefonoPadresG || "No nombre",
-            emailPadres: req.body.emailPadresG || "No nombre",
-            fechaHoy: req.body.fechaHoyG || "0000/00/00"
+            nombreAlumno: req.body.nombreAlumno || "Alumno no indicado",
+            grupoAlumno: req.body.grupoAlumno || "Grupo no indicado",
+            nombreProfesor: req.body.nombreProfesor || "Nombre profesor no indicado",
+            horarioAtencion: req.body.horarioAtencion || "Horario atencion no indicado",
+            fechaIncidente: req.body.fechaIncidente || "0000-00-00",
+            horaIncidente: req.body.horaIncidente || "Hora indicente no indicada",
+            lugarIncidente: req.body.lugarIncidente || "Sitio incidente no indicado",
+            tipificacion: req.body.tipificacion || "Tipificacion no seleccionada",
+            telefonoPadres: req.body.telefonoPadresG || "Telefono padres no indicado",
+            emailPadres: req.body.emailPadresG || "Email no indicado",
+            fechaHoy: req.body.fechaHoyG || "0000-00-00"
         })
 
         convivencia.save().then(data => {
